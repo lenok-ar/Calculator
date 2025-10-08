d47e8d57c2ecfe9626827641e280d222a5b7e43f
lenok-ar <leno4ka279a@gmail.com>
2025-10-09 01:46:25 +0700

    Add files via upload

---
+++ Method_Gaussa.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+class Method_Gaussa
+{
+    private double[,] _matrixA;
+    private double[] _vectorb;
+    private double[] _vectorB;
+
+    public Method_Gaussa(double[,] matrixA, double[] vectorB)
+    {
+        _matrixA = matrixA;
+        _vectorb = vectorB;
+        ConvertEquation();
+    }
+
+    private double[] ConvertEquation()
+    {
+        _vectorB = new double[_vectorb.Length];
+
+        for (int i = 0; i < _vectorb.Length; ++i)
+        {
+            _vectorB[i] = -(_vectorb[i]);
+        }
+
+        return _vectorB;
+    }
+
+    public double[] SolveGauss()
+    {
+        int n = _vectorB.Length;
+
+        for (int pivot = 0; pivot < n; ++pivot)
+        {
+            int maxRow = pivot;
+            double maxVal = Math.Abs(_matrixA[pivot, pivot]);
+
+            for (int i = pivot + 1; i < n; ++i)
+            {
+                if (Math.Abs(_matrixA[i, pivot]) > maxVal)
+                {
+                    maxVal = Math.Abs(_matrixA[i, pivot]);
+                    maxRow = i;
+                }
+            }
+
+            if (maxRow != pivot)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    double temp = _matrixA[pivot, j];
+                    _matrixA[pivot, j] = _matrixA[maxRow, j];
+                    _matrixA[maxRow, j] = temp;
+                }
+
+                double tempB = _vectorB[pivot];
+                _vectorB[pivot] = _vectorB[maxRow];
+                _vectorB[maxRow] = tempB;
+            }
+
+            for (int i = pivot + 1; i < n; ++i)
+            {
+                double factor = _matrixA[i, pivot] / _matrixA[pivot, pivot];
+
+                for (int j = pivot; j < n; ++j)
+                {
+                    _matrixA[i, j] -= factor * _matrixA[pivot, j];
+                }
+
+                _vectorB[i] -= factor * _vectorB[pivot];
+            }
+        }
+
+        double[] x = new double[n];
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            double sum = 0;
+
+            for (int j = i + 1; j < n; ++j)
+            {
+                sum += _matrixA[i, j] * x[j];
+            }
+
+            x[i] = (_vectorB[i] - sum) / _matrixA[i, i];
+        }
+
+        return x;
+    }
+}
---
+++ Method_JardanGaussa.cs
@@ -0,0 +1,71 @@
+﻿class Method_JardanGaussa
+{
+    private double[,] _martixA;
+    private double[] _vectorB;
+
+    public Method_JardanGaussa(double[,] martixA, double[] vectorB)
+    {
+        _martixA = martixA;
+        _vectorB = vectorB;
+    }
+
+    public double[] Solve()
+    {
+        int n = _martixA.GetLength(0);
+
+        double[,] matrix = new double[n, n + 1];
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                matrix[i, j] = _martixA[i, j];
+            }
+
+            matrix[i, n] = -(_vectorB[i]);
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (matrix[i, i] == 0)
+            {
+                for (int k = i + 1; k < n; ++k)
+                {
+                    if (matrix[k, i] != 0)
+                    {
+                        for (int j = 0; j <= n; ++j)
+                        {
+                            double temp = matrix[i, j];
+                            matrix[i, j] = matrix[k, j];
+                            matrix[k, j] = temp;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            double pivot = matrix[i, i];
+
+            for (int j = 0; j <= n; ++j)
+                matrix[i, j] /= pivot;
+
+            for (int k = 0; k < n; ++k)
+            {
+                if (k == i) continue;
+
+                double factor = matrix[k, i];
+
+                for (int j = 0; j <= n; ++j)
+                    matrix[k, j] -= factor * matrix[i, j];
+            }
+        }
+
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; ++i)
+            result[i] = matrix[i, n];
+
+        return result;
+    }
+}
